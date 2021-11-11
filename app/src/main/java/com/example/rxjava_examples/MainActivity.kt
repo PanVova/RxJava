50d0339fde9e298a7bd1e5d749d99e144308bee2
samurai <panvovandrik@gmail.com>
2021-11-11 13:02:36 +0200

    Add RxJava2 dependencies

--- app/src/main/java/com/example/rxjava_examples/MainActivity.kt
+++ app/src/main/java/com/example/rxjava_examples/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.rxjava_examples
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import io.reactivex.Observable
+import io.reactivex.Single
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }
